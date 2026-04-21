3c4485a074ecdec6f29ce95479235a1ac3d60db6
bastian <bast.floress@duocuc.cl>
2026-04-21 19:14:16 -0400

    Primer commit

---
+++ resenia/resenia/src/test/java/com/example/resenia/ReseniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReseniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
