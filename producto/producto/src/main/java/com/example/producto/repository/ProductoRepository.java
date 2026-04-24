e5fd16e03a55f8ef5e3def343aa1962851507f7b
Edu <edua.aguirre@duocuc.cl>
2026-04-24 18:17:19 -0400

    micro servicio producto

---
+++ producto/producto/src/main/java/com/example/producto/repository/ProductoRepository.java
@@ -0,0 +1,15 @@
+package com.example.producto.repository;
+
+import com.example.producto.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findByActivoTrue();
+
+    List<Producto> findByCategoriaIdAndActivoTrue(Long categoriaId);
+
+    List<Producto> findByNombreContainingIgnoreCaseAndActivotrue(String nombre);
+}
